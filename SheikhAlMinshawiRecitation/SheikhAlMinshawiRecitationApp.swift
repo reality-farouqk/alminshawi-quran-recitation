e33e8b6b4b24bdea49fc0f75a023eb7e9aa8dcfe
Umar sulaiman mailafiya <mailafiyaumarsulaiman@gmail.com>
2025-12-25 20:03:57 +0100

    Initial Commit

---
+++ SheikhAlMinshawiRecitation/SheikhAlMinshawiRecitationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheikhAlMinshawiRecitationApp.swift
+//  SheikhAlMinshawiRecitation
+//
+//  Created by UmarFarouqk on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SheikhAlMinshawiRecitationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
